d2e2212ca7cd02d96c097b9de1aeda8f33f19219
Maiakr <108643768+Maiakr@users.noreply.github.com>
2022-12-30 15:21:19 -0300

    Creación de Carpeta Front End
    Se Creo la carpeta FRONTEND y se coloco el proyecto adentro

---
+++ ProyectoIntegrador_MK/FRONTEND/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { FormsModule } from '@angular/forms';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        LogoAPComponent,
+        BannerComponent,
+        AcercaDeComponent,
+        ExperienciaComponent,
+        EducacionComponent,
+        SkillsComponent,
+        ProyectoComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgCircleProgressModule.forRoot({}),
+    ]
+})
+export class AppModule { }
