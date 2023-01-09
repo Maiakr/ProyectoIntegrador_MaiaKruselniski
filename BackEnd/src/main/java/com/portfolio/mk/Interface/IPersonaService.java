a34c0c9b0a8710b9dc247dec306fb5fafa260a77
Maiakr <108643768+Maiakr@users.noreply.github.com>
2023-01-09 09:23:44 -0300

    no se crean las tablas de la clase Persona, creacion de rama paralela Desarrollo
    No puedo iniciar a aplicacion de Tom Cat, siendo que antes lo pude hacer

--- BackEnd/src/main/java/com/portfolio/mk/Interface/IPersonaService.java
+++ BackEnd/src/main/java/com/portfolio/mk/Interface/IPersonaService.java
@@ -1,18 +1,19 @@
 package com.portfolio.mk.Interface;
 import com.portfolio.mk.Entity.Persona;
+
 import java.util.List;
 
 public interface IPersonaService {
     //Traer una lista de personas
-    public List<Persona> getPersona();
+  List<Persona> getPersona();
 
     //Guardar un objeto de tipo Persona
-    public void savePersona(Persona persona);
+    void savePersona(Persona persona);
 
     //Eliminar un objeto, pero lo buscamos por ID
-    public void deletePersona(Long id);
+  void deletePersona(Long id);
 
     //Buscar una persona por ID
-    public Persona findPersona(Long id);
+    Persona findPersona(Long id);
 
 }
