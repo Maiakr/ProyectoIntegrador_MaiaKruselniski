823bb8569a4eb7541304ab41dac7c36ce9963648
Maiakr <108643768+Maiakr@users.noreply.github.com>
2023-02-11 15:25:08 -0300

    creación clase dtoPersona

--- BackEnd/src/main/java/com/portfolio/mk/Repository/IPersonaRepository.java
+++ BackEnd/src/main/java/com/portfolio/mk/Repository/IPersonaRepository.java
@@ -4,7 +4,10 @@ import com.portfolio.mk.Entity.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IPersonaRepository extends JpaRepository<Persona, Long> {
-
+    public Optional<Persona> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
 }
